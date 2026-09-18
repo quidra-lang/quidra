8859a5f611d00a45eca0617a792d430e043befc5
Hiroki Kobayashi <56967584+koba-jon@users.noreply.github.com>
2026-09-18 14:32:20 +0900

    feat: represent explicit tensor device placement in IR

--- include/quidra/ir.hpp
+++ include/quidra/ir.hpp
@@ -56,7 +56,8 @@ struct BytesGet { ValueId out; ValueId bytes; ValueId index; std::uint32_t line{
 struct BytesSet { ValueId bytes; ValueId index; ValueId value; std::uint32_t line{}; std::uint32_t column{}; bool bounds_proven{}; };
 struct NumericConvert { ValueId out; ValueId value; Type source_type; Type target_type; bool checked_range{}; std::uint32_t line{}; std::uint32_t column{}; };
 struct ArrayNumericCast { ValueId out; ValueId array; Type source_type; Type target_type; std::uint32_t line{}; std::uint32_t column{}; };
-struct TensorCreate { ValueId out; ValueId shape; Type type; int fill_mode{}; std::uint32_t line{}; std::uint32_t column{}; };
+struct TensorCreate { ValueId out; ValueId shape; std::optional<ValueId> gpu; Type type; int fill_mode{}; std::uint32_t line{}; std::uint32_t column{}; };
+struct TensorTransfer { ValueId out; ValueId tensor; std::optional<ValueId> gpu; Type type; std::uint32_t line{}; std::uint32_t column{}; };
 struct TensorReshape { ValueId out; ValueId tensor; ValueId shape; Type type; std::uint32_t line{}; std::uint32_t column{}; };
 struct TensorContiguous { ValueId out; ValueId tensor; Type type; };
 struct TensorShape { ValueId out; ValueId tensor; Type type; };
@@ -242,7 +243,7 @@ using Instruction = std::variant<ConstantInt, ConstantFloat, ConstantBool, Const
                                  StringUtf8, StringCodepoints, StringJoin, StringConcat,
                                  StringCanAppendMove, StringAppendMove,
                                  BytesAlloc, BytesLength, BytesGet, BytesSet,
-                                 NumericConvert, ArrayNumericCast, TensorCreate, TensorReshape, TensorContiguous,
+                                 NumericConvert, ArrayNumericCast, TensorCreate, TensorTransfer, TensorReshape, TensorContiguous,
                                  TensorShape, TensorIsContiguous, TensorItem, TensorCast,
                                  ShapedConstraintCheck, ExtentEqualCheck, NeuralNumericCast,
                                  NeuralTrack, NeuralUntrack, NeuralUnary, NeuralBinary, NeuralGrad,

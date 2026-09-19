e20569c18e64606547eef1a78dde30f8e6ea8f5a
Hiroki Kobayashi <56967584+koba-jon@users.noreply.github.com>
2026-09-19 17:45:45 +0900

    runtime: expose direct text buffer construction

--- src/runtime_internal.hpp
+++ src/runtime_internal.hpp
@@ -13,5 +13,10 @@ extern "C" char* quidra_runtime_copy_validated_text_bytes(
     const char* data, unsigned long long size);
 extern "C" char* quidra_runtime_try_copy_text_bytes(
     const char* data, unsigned long long size);
+extern "C" char* quidra_runtime_allocate_text_buffer(
+    unsigned long long size);
+extern "C" bool quidra_runtime_commit_text_buffer(
+    char* data, unsigned long long size);
+extern "C" void quidra_managed_release(void* value, void* drop_function);
 extern "C" unsigned long long quidra_runtime_text_byte_length(
     const char* text);
